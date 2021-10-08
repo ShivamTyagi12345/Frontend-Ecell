aa822afb776b2d9c0a2a3752bd2e8f95a3f1f6d1
Rajan kumar <krcpr007@gmail.com>
2021-10-08 14:06:53 +0530

    Added Footer component in App.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,217 @@
 import React from "react";
-
-
+import './about.css';
 const About = () => {
     return (
-        <div>
-            <h3 >Thanks For ABOUT START Working !</h3>
-  
+        <div className="vision">
+            <img className="img-fluid" src="https://source.unsplash.com/1536x400/?notebook,pen" alt="top-img" />
+            <div className="container" style={{ paddingTop: "30px", color: "#393939" }}>
+                <h1 style={{textAlign: "initial"  }}>Vision</h1>
+                <div className="vision-text" style={{ opacity: .6 }}>
+                    <div className="line"></div>
+                    <p style={{ marginTop: "50px" }}>Our vision is to make Tinkering Lab the fountainhead of plethora of technological advancements. With a zeal to work on cutting-edge technologies, we aim to provide world-class services to build the most jaw-dropping projects. To move forward on this path, we have all the different machines and equipments which need to be used. This motivates us towards our end goal and we use this self motivation as a weapon to get further in life and convert our dreams into reality. Our end goal is to make Tinkering Lab a source of pride for the nation.</p>
+                    <p style={{ marginTop: "15px" }}>With an establishment of Tinkering Lab, the Indian Institute of Technology Roorkee is poised to join a revolution that aims at converting young minds into thinkers and innovators. The mission of Tinkering Lab is not only to strengthen the bond between industry experts and youth but also to strive towards overall development of students, the institute, society, and the country. This can be achieved by creating an ambience which fosters research and innovation that Tinkering Lab provides. The lab is a way to encourage a problem-solving attitude and to enkindle the brightest minds of this country.</p>
+                    <p style={{ marginTop: "15px", marginBottom: "100px" }}>In the context of education, the Tinkering Lab refers to space where the youngsters can experiment with, learn, develop and conceptualize different scientific ideas. It is a lot similar to DIY(Do-it-yourself) method where the students are encouraged to apply the knowledge that they have gained in the classroom.</p>
+                </div>
+                <div className="team">
+                    <h1 style={{textAlign:"initial"}}>Team</h1>
+                    <div className="vision-text">
+                        <div className="line"></div>
+                        <div className="head" style={{ marginTop: "10px", marginBottom: "10px", opacity: .7 }}>Mangement</div>
+                        <div className="row">
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5>Rajan Kumar</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card m-2 " style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5>Rajan Kumar</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF27_okMDcJVw/profile-displayphoto-shrink_400_400/0/1621483176011?e=1639008000&v=beta&t=K-H6rdpAF_X7v9203ha-hiyfHUrjjq3WFHvaza0EWjc" className="card-img-top " alt="..." />
+                                <div className="card-body">
+                                    <h5>Rajan Kumar</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5>Rajan Kumar</h5>
+                                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="head" style={{ marginTop: "10px", marginBottom: "10px", opacity: .7 }}>Students’ Body</div>
+                        <div className="row row-md-2">
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top " alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                       <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                    <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card m-2" style={{ width: "17rem" }}>
+                                <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF27_okMDcJVw/profile-displayphoto-shrink_400_400/0/1621483176011?e=1639008000&v=beta&t=K-H6rdpAF_X7v9203ha-hiyfHUrjjq3WFHvaza0EWjc" className="card-img-top img-fluid" alt="..." />
+                                <div className="card-body">
+                                    <h4 className="text-center">Rajan Kumar</h4>
+                                    <div className="">
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-facebook-square"></i> </a>
+                                        <a href="\"> <i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-linkedin"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-github"></i></a>
+                                        <a href="\"><i style={{ fontSize: "35px", margin: "12px" }} className="fab fa-twitter"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
