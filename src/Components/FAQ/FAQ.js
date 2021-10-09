a00b5b2946b46cf8bdc57c3acce9d23ad5290b6e
ShivamTyagi <shivamt.ug19.ec@nitp.ac.in>
2021-10-09 18:49:42 +0530

    Initial FAQ

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import "./styles.css";
+import { FiPlus } from "react-icons/fi";
 
-const FAQ = () => {
-    return (
-        <div>
-            strt working here 
+export default function FAQ() {
+  const [active, setActive] = useState(false);
+
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    contentRef.current.style.maxHeight = active
+      ? `${contentRef.current.scrollHeight}px`
+      : "0px";
+  }, [contentRef, active]);
+
+  const toggleAccordion = () => {
+    setActive(!active);
+  };
+  return (
+    <>
+      <div className="FAQ">
+        <div className="Question1">
+          <button
+            className={`question-section ${active}`}
+            onClick={toggleAccordion}
+          >
+            <div>
+              <div className="question-align">
+                <h4 className="question-style">
+                  Why do you like web developemnt
+                </h4>
+                <FiPlus
+                  className={active ? `question-icon rotate` : `question-icon`}
+                />
+              </div>
+              <div
+                ref={contentRef}
+                className={active ? `answer answer-divider` : `answer`}
+              >
+                <p>Because I love coding</p>
+              </div>
+            </div>
+          </button>
         </div>
-    )
-}
 
-export default FAQ
+        
+      </div>
+    </>
+  );
+}
