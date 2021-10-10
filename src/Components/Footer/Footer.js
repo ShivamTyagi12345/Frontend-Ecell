e52bdb3919767eb7372f5e9964da018e82aaa5a2
Harsh Anand <75748840+its-me-Harsh-Anand@users.noreply.github.com>
2021-10-10 22:36:48 +0530

    checking footer again

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import FooterInfo from './FooterInfo'
+import './Footer.css'
+import FooterLogo from './FooterLogo'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <FooterInfo />
+            <FooterLogo />
+        </div>
+    )
+}
+
+export default Footer
